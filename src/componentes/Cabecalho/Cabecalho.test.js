fc77ac144a4ef0ae628e1f234fbf24cabe2fa987
David Carmo <davidsc2812@gmail.com>
2023-04-24 23:37:14 -0300

    test: add test to verify if header has user's name

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,8 @@
-test('Primeito teste', () => {
-  const numero = 10;
-  expect(numero).toBe(10);
+import { react, screen } from '@testing-library/react';
+import Cabecalho from '.';
+
+test('Deve renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuario).toBeInTheDocument();
 });
