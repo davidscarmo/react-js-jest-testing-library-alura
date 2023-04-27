3c479b350fb71f702c452bc8d6a154689eb2ec75
David Carmo <davidsc2812@gmail.com>
2023-04-26 23:28:21 -0300

    test: add tests using userEvent

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -22,3 +22,25 @@ describe('Deve renderizar um campo de input', () => {
     expect(campoTexto).toHaveValue(50);
   });
 });
+
+test('Deve chamar um evento de onSubmit ao clicar em realizar transacao', () => {
+  const realizarTransacao = jest.fn();
+
+  render(<Formulario realizarTransacao={realizarTransacao} />);
+  const botao = screen.getByRole('button');
+
+  userEvent.click(botao);
+  expect(realizarTransacao).toHaveBeenCalledTimes(1);
+});
+
+test('Deve ser possível selecionar uma opção do elemento <select/>', () => {
+  render(<Formulario />);
+  const select = screen.getByRole('combobox');
+  userEvent.selectOptions(select, ['Depósito']);
+
+  expect(
+    screen.getByRole('option', { name: 'Selecione um tipo de transação' })
+      .selected
+  ).toBe(false);
+  expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(true);
+});
